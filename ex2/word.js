7137336bb8274a8f0710af4321d18d740ce22c29
safaweslati <92674307+safaweslati@users.noreply.github.com>
2022-03-05 01:57:25 +0100

    Delete ex2 directory

--- ex2/word.js
+++
@@ -1,6 +0,0 @@
-const elements=document.querySelector("#change");
-const text=document.querySelector("p");
-elements.addEventListener("change",(e)=>{
-    text.style[e.target.id]=
-    e.target.id=="font-size" ? e.target.value+"px":e.target.value;
-})
