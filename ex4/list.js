fbaa03b827b50f239a95cece5939cedec515135e
safaweslati <92674307+safaweslati@users.noreply.github.com>
2022-03-05 01:57:48 +0100

    Delete ex4 directory

--- ex4/list.js
+++
@@ -1,8 +0,0 @@
-const getRandomColor= () => 
-   "#"+Math.floor(Math.random()*1755488).toString();
-const clickColorElement =(element)=>{
-    element.addEventListener("click",
-    (e)=>(e.target.style.color=getRandomColor()));
-
-};
-clickColorElement(document.querySelector("ol"));
